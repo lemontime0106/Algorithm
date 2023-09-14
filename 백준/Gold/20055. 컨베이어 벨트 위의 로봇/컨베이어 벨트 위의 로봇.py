392693bc3a5a36a383a34228fa17903c0f523161
SSAFY10kim <139415938+SSAFY10kim@users.noreply.github.com>
2023-09-14 17:49:58 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 1512 ms, Memory: 120460 KB -BaekjoonHub

---
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+N, K = map(int, input().split())
+lst = deque(list(map(int, input().split())))
+robot = deque([0] * N)
+cnt = 1
+
+while True:
+    # 벨트 돌리기
+    lst.appendleft(lst.pop())
+    robot.appendleft(robot.pop())
+    robot[N-1] = 0
+
+    # 로봇 움직이기
+    for i in range(N-1, -1, -1):
+        if robot[i] == 1 and robot[i+1] == 0 and lst[i+1] > 0:
+            robot[i], robot[i+1] = 0, 1
+            lst[i+1] -= 1
+    robot[N-1] = 0
+
+    # 로봇 올리기
+    if robot[0] == 0 and lst[0] > 0:
+        robot[0] = 1
+        lst[0] -= 1
+
+    # 종료조건 검사
+    if lst.count(0) >= K:
+        break
+    # 아니면 +1
+    cnt += 1
+
+# print(lst)
+# print(robot)
+print(cnt)
