51b0376246bc69a2eb419f99083b41bd4dbd51da
SSAFY10kim <139415938+SSAFY10kim@users.noreply.github.com>
2024-03-20 22:10:52 +0900

    [Silver I] Title: 미로 탐색, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -0,0 +1,27 @@
+n, m = map(int, input().split())
+graph = [list(map(int, input())) for _ in range(n)]
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(x, y):
+    q = []
+    q.append((x, y))
+    while q:
+        x, y = q.pop(0)
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx >= n or ny >= m or nx < 0 or ny < 0:
+                continue
+
+            if graph[nx][ny] == 0:
+                continue
+
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                q.append((nx, ny))
+
+    return graph[n-1][m-1]
+
+print(bfs(0, 0))
