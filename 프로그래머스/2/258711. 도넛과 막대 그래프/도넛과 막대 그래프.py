bd382ed919825acb58b73be513d35345817f3294
SSAFY10kim <139415938+SSAFY10kim@users.noreply.github.com>
2024-05-24 14:54:53 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.py
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.py
@@ -1,27 +1,3 @@
 def solution(edges):
-    # 생성된 노드 : 들어오는거 X, 나가는거 2개 이상
-    # 도넛 : in, out : 1, 1
-    # 막대 : in, out : 0, 1
-    # 8자 : in, out : 2, 2
-    
-    n = len(edges) + 1
-    answer = [[0, 0] for _ in range(n + 1)]
-    sol = [0, 0, 0, 0]
-    
-    for edge in edges:
-        a, b = edge[0], edge[1]
-        answer[a][0] += 1
-        answer[b][1] += 1
-    
-    for i in range(1, n + 1):
-        in_out = answer[i]
-        if in_out[0] >= 2 and in_out[1] == 0:
-            sol[0] = i
-        elif in_out[0] == 0 and in_out[1] > 0:
-            sol[2] += 1
-        elif in_out[0] >= 2 and in_out[1] >= 2:
-            sol[3] += 1
-    
-    sol[1] = answer[sol[0]][0] - sol[2] - sol[3]
-    
-    return sol
+    answer = []
+    return answer
