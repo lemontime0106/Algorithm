9aa21dc5c1fa5810a01cc295e10024ad6ebf7d58
SSAFY10kim <139415938+SSAFY10kim@users.noreply.github.com>
2024-06-16 13:26:58 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 62.23 ms, Memory: 12.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.py
@@ -0,0 +1,19 @@
+def solution(arr):
+    answer = [0, 0]
+    length = len(arr)
+    
+    def quad(a, b, l):
+        start = arr[a][b]
+        for i in range(a, a+l):
+            for j in range(b, b+l):
+                if arr[i][j] != start:
+                    l=l//2
+                    quad(a, b, l)
+                    quad(a, b+l, l)
+                    quad(a+l, b, l)
+                    quad(a+l, b+l, l)
+                    return
+        answer[start] += 1
+        
+    quad(0, 0, length)
+    return answer
