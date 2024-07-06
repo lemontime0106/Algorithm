dba12506b073fe65075029d719df16fd99687146
lemontime <139415938+SSAFY10kim@users.noreply.github.com>
2024-07-06 15:38:08 +0900

    [level 3] Title: 단어 변환, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+def bfs(b, t, w):
+    q = deque()
+    q.append([b, 0])
+    
+    while q:
+        now, cnt = q.popleft()
+        
+        if now == t:
+            return cnt
+        
+        for i in w:
+            temp = 0
+            for j in range(len(now)):
+                if now[j] != i[j]:
+                    temp += 1
+            if temp == 1:
+                q.append([i, cnt + 1])
+
+def solution(begin, target, words):
+    if target not in words:
+        return 0
+    else:
+        return bfs(begin, target, words)
+
+        
+
+
+        
