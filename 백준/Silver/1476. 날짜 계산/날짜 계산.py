0ba3b72e5f7b1544c0b9f1ec67f127576575fa69
lemontime <139415938+SSAFY10kim@users.noreply.github.com>
2024-07-31 11:19:35 +0900

    [Silver V] Title: 날짜 계산, Time: 120 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Silver/1476. 날짜 계산/날짜 계산.py
@@ -0,0 +1,9 @@
+E, S, M = map(int, input().split())
+answer = 1
+
+while True:
+    if((answer - E) % 15 == 0) and ((answer - S) % 28 == 0) and ((answer - M) % 19 == 0):
+        break
+    answer += 1
+
+print(answer)
