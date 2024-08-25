1a2a2e7ef91a34b6e2d30d4171e047e7db6d2085
lemontime <139415938+SSAFY10kim@users.noreply.github.com>
2024-08-26 01:25:25 +0900

    [level 2] Title: 할인 행사, Time: 144.82 ms, Memory: 17.4 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,3 +1,23 @@
 def solution(want, number, discount):
-    answer = 0
-    return answer
+    ans = 0
+    N = len(want)
+    want_dict = {}
+    for i in range(N):
+        want_dict[want[i]] = number[i]
+    
+    M = len(discount)
+    days = sum(number)
+    
+    for i in range(M-days+1):
+        cart = {}
+        for j in range(days):
+            item = discount[i + j]
+            if item not in cart:
+                cart[item] = 1
+            else:
+                cart[item] += 1
+        
+        if cart == want_dict:
+            ans += 1
+
+    return ans
