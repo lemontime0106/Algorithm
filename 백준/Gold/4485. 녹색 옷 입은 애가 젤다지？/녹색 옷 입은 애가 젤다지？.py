bb3a5775eadfdab0d3fd99a6d22d2726825c1469
lemontime <139415938+SSAFY10kim@users.noreply.github.com>
2024-09-11 01:22:23 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 164 ms, Memory: 112540 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
@@ -10,24 +10,22 @@ while True:
     else:
         T += 1
         MAP = [list(map(int, input().split())) for _ in range(N)]
-        q = []
-
         distance = [[float("inf")] * N for _ in range(N)]
         distance[0][0] = MAP[0][0]
-
+        q = []
         heapq.heappush(q, (MAP[0][0], 0, 0))
 
         while q:
-            d, y, x = heapq.heappop(q)
-
-            if y == N-1 and x == N-1:
+            d, x, y = heapq.heappop(q)
+            
+            if x == N-1 and y == N-1:
                 print(f"Problem {T}: {d}")
                 break
 
             for i in direct:
-                ny, nx = y + i[1], x + i[0]
-                if 0 <= ny < N and 0 <= nx < N:
-                    cost = d + MAP[ny][nx]
-                    if  distance[ny][nx] > cost:
-                        distance[ny][nx] = d + MAP[ny][nx]
-                        heapq.heappush(q, (MAP[ny][nx] + d, ny, nx))
+                nx, ny = x + i[0], y + i[1]
+                if 0 <= nx < N and 0 <= ny < N:
+                    cost = d + MAP[nx][ny]
+                    if cost < distance[nx][ny]:
+                        distance[nx][ny] = cost
+                        heapq.heappush(q, (cost, nx, ny))
