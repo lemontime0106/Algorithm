17939c9e3a6ba6a6bde9ea4181e8b55683758f16
lemontime <139415938+SSAFY10kim@users.noreply.github.com>
2024-10-02 01:34:12 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 928 ms, Memory: 159240 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,33 +1,35 @@
 from collections import deque
 
 N, M, K, X = map(int, input().split())
+
 graph = [[] for _ in range(N+1)]
-distance = [0] * (N + 1)
-visited = [False] * (N + 1)
+
 
 for _ in range(M):
     a, b = map(int, input().split())
     graph[a].append(b)
 
-def bfs(start):
-    answer = []
-    q = deque([start])
-    visited[start] = True
-    distance[start] = 0
+visited = [-1] * (N+1)
+visited[X] = 0
+
+def bfs(x):
+    q = deque([x])
+
     while q:
-        now = q.popleft()
-        for i in graph[now]:
-            if not visited[i]:
-                visited[i] = True
+        node = q.popleft()
+
+        for i in graph[node]:
+            if visited[i] == -1:
+                visited[i] = visited[node] + 1
                 q.append(i)
-                distance[i] = distance[now] + 1
-                if distance[i] == K:
-                    answer.append(i)
-    if len(answer) == 0:
-        print(-1)
-    else:
-        answer.sort()
-        for i in answer:
-            print(i, end="\n")
+
 
 bfs(X)
+
+answer = [i for i in range(1, N+1) if visited[i] == K]
+
+if answer:
+    for city in answer:
+        print(city)
+else:
+    print(-1)
