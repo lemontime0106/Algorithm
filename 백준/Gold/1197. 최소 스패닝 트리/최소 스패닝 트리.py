49a3649e026794e90d3baaa430ef3f040e46d5e5
lemontime <139415938+SSAFY10kim@users.noreply.github.com>
2024-10-29 00:08:35 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 352 ms, Memory: 254136 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -0,0 +1,38 @@
+import sys
+sys.setrecursionlimit(100000)
+
+V, E = map(int, input().split())
+edges = []
+
+for _ in range(E):
+    a, b, c = map(int, input().split())
+    edges.append((a, b, c))
+
+edges.sort(key=lambda x: x[2])
+
+parent = [i for i in range(V+1)]
+
+def find_parent(x):
+    while parent[x] != x:
+        parent[x] = parent[parent[x]]
+        x = parent[x]
+    return x
+
+def union_parent(a, b):
+    a = find_parent(a)
+    b = find_parent(b)
+
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+def same_parent(a, b):
+    return find_parent(a) == find_parent(b)
+
+answer = 0
+for a, b, cost in edges:
+    if not same_parent(a, b):
+        union_parent(a, b)
+        answer += cost
+print(answer)
