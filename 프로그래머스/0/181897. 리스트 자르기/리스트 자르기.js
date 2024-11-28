d658c9ce0bfaecf3e0ac2fd47a9217987c8e6870
lemontime <139415938+lemontime0106@users.noreply.github.com>
2024-11-29 00:23:40 +0900

    [level 0] Title: 리스트 자르기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
@@ -0,0 +1,25 @@
+function solution(n, slicer, num_list) {
+    var answer = [];
+    
+    const [a, b, c] = slicer
+    
+    if (n === 1) {
+        for (let i = 0; i <= b; i++) {
+            answer.push(num_list[i])
+        }
+    } else if (n === 2) {
+        for (let i = a; i < num_list.length; i++) {
+            answer.push(num_list[i])
+        }
+    } else if (n === 3) {
+        for (let i = a; i <= b; i++) {
+            answer.push(num_list[i])
+        }
+    } else if (n === 4) {
+        for (let i = a; i <= b; i += c) {
+            answer.push(num_list[i])
+        }
+    }
+    
+    return answer;
+}
