d0f8ffa016520fe5474b115f094c21cd4da6b527
lemontime <139415938+lemontime0106@users.noreply.github.com>
2024-12-04 01:38:14 +0900

    [level 0] Title: 배열의 원소 삭제하기, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.js
@@ -0,0 +1,11 @@
+function solution(arr, delete_list) {
+    var answer = [];
+    
+    for (let i of arr) {
+        if (!delete_list.includes(i)) {
+            answer.push(i)
+        }
+    }
+    
+    return answer;
+}
