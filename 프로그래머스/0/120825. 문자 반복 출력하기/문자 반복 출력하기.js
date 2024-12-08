9252728a766114ec5a936d1324914448395459fa
lemontime <139415938+lemontime0106@users.noreply.github.com>
2024-12-09 00:22:53 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -0,0 +1,11 @@
+function solution(my_string, n) {
+    var answer = '';
+    
+    for (let str of my_string) {
+        for (let i=0; i < n; i++) {
+            answer += str
+        }
+    }
+    
+    return answer;
+}
