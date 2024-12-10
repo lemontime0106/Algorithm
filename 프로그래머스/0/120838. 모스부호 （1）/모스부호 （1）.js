6cb75fc9218e47f9a9a8fc596702c7dfd2c748d6
lemontime <139415938+lemontime0106@users.noreply.github.com>
2024-12-11 00:45:58 +0900

    [level 0] Title: 모스부호 (1), Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,21 @@
+function solution(letter) {
+    var answer = '';
+    const morse = {
+        '.-': 'a', '-...': 'b', '-.-.': 'c', '-..': 'd', '.': 'e',
+        '..-.': 'f', '--.': 'g', '....': 'h', '..': 'i', '.---': 'j',
+        '-.-': 'k', '.-..': 'l', '--': 'm', '-.': 'n', '---': 'o',
+        '.--.': 'p', '--.-': 'q', '.-.': 'r', '...': 's', '-': 't',
+        '..-': 'u', '...-': 'v', '.--': 'w', '-..-': 'x', '-.--': 'y',
+        '--..': 'z'
+    };
+
+    const codes = letter.split(' ');
+
+    for (let code of codes) {
+        if (morse[code]) {
+            answer += morse[code];
+        }
+    }
+
+    return answer;
+}
