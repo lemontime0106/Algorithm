9d2180282d6165d3a89f5131dfa229bb94e89f8a
lemontime <139415938+lemontime0106@users.noreply.github.com>
2024-12-13 12:30:06 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.05 ms, Memory: 33 MB -BaekjoonHub

---
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -0,0 +1,12 @@
+function solution(sides) {
+    var answer = 0;
+    sides.sort((a, b) => a - b);
+    
+    if (sides[0] + sides[1] > sides[2]) {
+        return 1
+    } else {
+        return 2
+    }
+    
+    return answer;
+}
