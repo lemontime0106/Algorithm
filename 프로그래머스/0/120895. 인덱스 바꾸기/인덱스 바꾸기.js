ce6ec4bb1725933709bbe04477f1b74f85563395
lemontime <139415938+lemontime0106@users.noreply.github.com>
2024-12-14 16:03:17 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,9 @@
+function solution(my_string, num1, num2) {
+    var answer = [...my_string];
+    
+    let temp = answer[num1]
+    answer[num1] = answer[num2]
+    answer[num2] = temp
+    
+    return answer.join("");
+}
