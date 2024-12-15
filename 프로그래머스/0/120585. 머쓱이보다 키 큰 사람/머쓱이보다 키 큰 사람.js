a6f2077a2257c96ef4717cd3394b21a040ffdfc3
lemontime <139415938+lemontime0106@users.noreply.github.com>
2024-12-16 00:54:57 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -0,0 +1,11 @@
+function solution(array, height) {
+    var answer = 0;
+    
+    for (let i of array) {
+        if (i > height) {
+            answer++
+        }
+    }
+    
+    return answer;
+}
