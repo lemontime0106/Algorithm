1fef76170a746374b5d13e4fabd4804ad10855d5
lemontime <139415938+lemontime0106@users.noreply.github.com>
2024-12-18 21:37:11 +0900

    [level 3] Title: 섬 연결하기, Time: 0.07 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/README.md
+++ 프로그래머스/3/42861. 섬 연결하기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 섬 연결하기 - 42861 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861#qna) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.03 ms
+메모리: 10 MB, 시간: 0.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 15일 12:44:20
+2024년 12월 18일 21:37:15
 
 ### 문제 설명
 
--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,14 +1,32 @@
-def solution(n, costs):
-    answer = 0
-    costs.sort(key= lambda x: x[2])
-    link = set([costs[0][0]])
+def find_parent(x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent[x])
+    return parent[x]
+
+def union(a, b):
+    a = find_parent(a)
+    b = find_parent(b)
     
-    while len(link) != n:
-        for v in costs:
-            if v[0] in link and v[1] in link:
-                continue
-            if v[0] in link or v[1] in link:
-                link.update([v[0], v[1]])
-                answer += v[2]
-                break
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+def solution(n, costs):
+    global parent
+    
+    answer = 0
+    parent = [i for i in range(n)]
+    
+    edges = []
+    for a, b, c in costs:
+        edges.append((c, a, b))
+    
+    edges.sort()
+    
+    for c, a, b in edges:
+        if find_parent(a) != find_parent(b):
+            union(a, b)
+            answer += c
+    
     return answer
