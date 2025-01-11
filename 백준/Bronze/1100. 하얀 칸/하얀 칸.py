ecaafb40d930370f2d9153f90918d4232d33cf6f
lemontime <139415938+lemontime0106@users.noreply.github.com>
2025-01-12 00:22:04 +0900

    [Bronze II] Title: 하얀 칸, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.py
@@ -0,0 +1,9 @@
+MAP = [list(input()) for _ in range(8)]
+answer = 0
+
+for i in range(8):
+    for j in range(8):
+        if (i + j) % 2 == 0 and MAP[i][j] == "F":
+            answer += 1
+
+print(answer)
