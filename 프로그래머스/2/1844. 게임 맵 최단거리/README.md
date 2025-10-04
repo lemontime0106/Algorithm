6980281882092db693b7adbbbf6373f3a5c81a05
lemontime <139415938+lemontime0106@users.noreply.github.com>
2025-10-05 01:33:22 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.24 ms, Memory: 9.39 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=python3) 
 
 ### 성능 요약
 
-메모리: 37.4 MB, 시간: 35.69 ms
+메모리: 9.39 MB, 시간: 9.24 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 18일 13:41:40
+2025년 10월 05일 01:33:20
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,26 +1,30 @@
 from collections import deque
 
 def solution(maps):
+    answer = 0
+    
     N = len(maps)
     M = len(maps[0])
-    q = deque()
-    q.append((0, 0))
-    visited = [[0] * M for _ in range(N)]
-    visited[0][0] = 1
+    
+    visited = [[False for _ in range(M)] for _ in range(N)]
     
     direct = [(-1, 0), (1, 0), (0, -1), (0, 1)]
     
+    q = deque([(0, 0)])
+    visited[0][0] = True
+    
     while q:
         x, y = q.popleft()
-        for d in direct:
-            nx, ny = x + d[0], y + d[1]
-            if 0 <= nx < N and 0 <= ny < M and maps[nx][ny] == 1 and visited[nx][ny] == 0:
-                visited[nx][ny] = visited[x][y] + 1
-                q.append((nx, ny))
+        for dx, dy in direct:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < N and 0 <= ny < M and maps[nx][ny] == 1:
+                if not visited[nx][ny]:
+                    visited[nx][ny] = True
+                    q.append((nx, ny))
+                    maps[nx][ny] = maps[x][y] + 1
+                    
+    if maps[N-1][M-1] == 1:
+        return -1
     
-    if visited[N-1][M-1] == 0:
-        answer = -1
-    else:
-        answer = visited[N-1][M-1]
-    
-    return answer
+    elif maps[N-1][M-1] != 1:
+        return maps[N-1][M-1]
