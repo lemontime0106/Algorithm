a61347e41c3357c800b7f91ab2f35b621e76124d
lemontime <139415938+lemontime0106@users.noreply.github.com>
2025-10-05 17:55:21 +0900

    [Silver II] Title: DFS와 BFS, Time: 176 ms, Memory: 123196 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -2,43 +2,34 @@ from collections import deque
 
 N, M, V = map(int, input().split())
 
-graph = [[] for _ in range(N + 1)]
-
+MAP = [[0] * (N + 1) for _ in range(N + 1)]
 for _ in range(M):
     a, b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
-    graph[a].sort()
-    graph[b].sort()
-
-# print(graph)
+    MAP[a][b] = 1
+    MAP[b][a] = 1
 
 visited_dfs = [False] * (N + 1)
 visited_bfs = [False] * (N + 1)
 
-dfs_list = []
-bfs_list = []
-
-def dfs(start):
-    visited_dfs[start] = True
-    dfs_list.append(start)
-    for i in graph[start]:
-        if not visited_dfs[i]:
+def dfs(v):
+    visited_dfs[v] = True
+    print(v, end=' ')
+    for i in range(1, N + 1):
+        if MAP[v][i] == 1 and not visited_dfs[i]:
             dfs(i)
 
-def bfs(start):
-    q = deque([start])
-    visited_bfs[start] = True
+def bfs(v):
+    q = deque([v])
+    visited_bfs[v] = True
+
     while q:
         v = q.popleft()
-        bfs_list.append(v)
-        for i in graph[v]:
-            if not visited_bfs[i]:
+        print(v, end=' ')
+        for i in range(1, N + 1):
+            if MAP[v][i] == 1 and not visited_bfs[i]:
                 q.append(i)
                 visited_bfs[i] = True
 
 dfs(V)
-bfs(V)
-print(*dfs_list, end=' ')
 print()
-print(*bfs_list, end=' ')
+bfs(V)
