99257766324fc84a7920d62bd118800b7d298441
lemontime <139415938+lemontime0106@users.noreply.github.com>
2026-01-24 02:10:33 +0900

    [Silver I] Title: 스타트와 링크, Time: 444 ms, Memory: 113580 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,32 +1,34 @@
 N = int(input())
+
 MAP = [list(map(int, input().split())) for _ in range(N)]
 
 visited = [False] * N
-answer = float("inf")
+answer= float("inf")
 
-def dfs(a, b):
+def dfs(idx, cnt):
     global answer
 
-    # 조기 종료 조건: 능력치 차이가 0이면 최적이므로 바로 종료
-    if answer == 0:
-        return
-
-    if a == N // 2:
+    if cnt == N//2:
         start, link = 0, 0
         for i in range(N):
-            for j in range(N):
+            for j in range(i+1, N):
                 if visited[i] and visited[j]:
-                    start += MAP[i][j]
+                    start += MAP[i][j] + MAP[j][i]
+
                 elif not visited[i] and not visited[j]:
-                    link += MAP[i][j]
+                    link += MAP[i][j] + MAP[j][i]
+
         answer = min(answer, abs(start - link))
         return
 
-    for i in range(b, N):
+    for i in range(idx, N):
         if not visited[i]:
             visited[i] = True
-            dfs(a+1, i+1)  # 탐색 시작 지점을 i+1로 조정하여 중복 탐색 방지
+            dfs(i + 1, cnt + 1)
             visited[i] = False
 
-dfs(0, 0)
+
+visited[0] = True
+dfs(1, 1)
+
 print(answer)
