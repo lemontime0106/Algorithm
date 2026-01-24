8c4ea8615a859ec961f5d08039970988540bb09e
lemontime <139415938+lemontime0106@users.noreply.github.com>
2026-01-25 00:31:30 +0900

    [Silver I] Title: 경로 찾기, Time: 128 ms, Memory: 112304 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.py
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -1,24 +1,24 @@
 from collections import deque
 
 N = int(input())
-graph = [list(map(int, input().split())) for _ in range(N)]
+MAP = [list(map(int, input().split())) for _ in range(N)]
 visited = [[0] * N for _ in range(N)]
 
 def bfs(x):
     q = deque()
     q.append(x)
-    check = [0] * N
+    lst = [0] * N
 
     while q:
-        n = q.popleft()
+        y = q.popleft()
 
         for i in range(N):
-            if check[i] == 0 and graph[n][i] == 1:
+            if lst[i] == 0 and MAP[y][i] == 1:
                 q.append(i)
-                check[i] = 1
+                lst[i] = 1
                 visited[x][i] = 1
 
-for i in range(0, N):
+for i in range(N):
     bfs(i)
 
 for i in visited:
