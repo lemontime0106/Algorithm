fd3fade70336b54be9060101f712d7d6d17995f7
lemontime <139415938+lemontime0106@users.noreply.github.com>
2026-01-24 13:25:13 +0900

    [Silver I] Title: 미로 탐색, Time: 124 ms, Memory: 112140 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,16 +1,18 @@
-N, M = map(int, input().split())
-MAP = [list(map(int, input())) for _ in range(N)]
+from collections import deque
 
-direct = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+N, M = map(int, input().split())
+
+MAP = [list(map(int, input().strip())) for _ in range(N)]
+
+direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
 def bfs(x, y):
-    q = []
-    q.append((x, y))
+    q = deque([(x, y)])
+
     while q:
-        x, y = q.pop(0)
-        for i in range(4):
-            nx = x + direct[i][0]
-            ny = y + direct[i][1]
+        x, y = q.popleft()
+        for dx, dy in direction:
+            nx, ny = x + dx, y + dy
             if 0 <= nx < N and 0 <= ny < M and MAP[nx][ny] == 1:
                 MAP[nx][ny] = MAP[x][y] + 1
                 q.append((nx, ny))
