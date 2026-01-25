a6927181ddf316b268bb3f5895e4bbb4de2eac3f
lemontime <139415938+lemontime0106@users.noreply.github.com>
2026-01-26 00:07:43 +0900

    [Silver I] Title: 그림, Time: 184 ms, Memory: 125016 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -3,35 +3,34 @@ from collections import deque
 N, M = map(int, input().split())
 
 MAP = [list(map(int, input().split())) for _ in range(N)]
-
 visited = [[False] * M for _ in range(N)]
-direct = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-lst = []
-def bfs(x, y):
-    cnt = 1
 
+direct = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+temp = []
+
+def bfs(x, y):
     q = deque()
     q.append((x, y))
     visited[x][y] = True
+    cnt = 1
+
     while q:
         x, y = q.popleft()
-        for d in direct:
-            nx, ny = x + d[0], y + d[1]
-            if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny] and MAP[nx][ny] == 1:
-                cnt += 1
-                visited[nx][ny] = True
-                q.append((nx, ny))
+        for dx, dy in direct:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < N and 0 <= ny < M:
+                if not visited[nx][ny] and MAP[nx][ny] == 1:
+                    visited[nx][ny] = True
+                    cnt += 1
+                    q.append((nx, ny))
+
     return cnt
 
 for i in range(N):
     for j in range(M):
-        if MAP[i][j] == 1 and not visited[i][j]:
-            a = bfs(i, j)
-            lst.append(a)
+        if not visited[i][j] and MAP[i][j] == 1:
+            temp.append(bfs(i, j))
 
-if len(lst) == 0:
-    print(0)
-    print(0)
-else:
-    print(len(lst))
-    print(max(lst))
+print(len(temp))
+print(max(temp) if temp else 0)
