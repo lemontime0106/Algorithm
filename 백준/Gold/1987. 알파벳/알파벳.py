b0b0eb4cfc6ac8d6a2ab58ccd19dd8ca1359ff0d
lemontime <139415938+lemontime0106@users.noreply.github.com>
2026-02-18 02:39:09 +0900

    [Gold IV] Title: 알파벳, Time: 6748 ms, Memory: 142912 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,27 +1,30 @@
-N, M = map(int, input().split())
+R, C = map(int, input().split())
 
-MAP = [list(input()) for _ in range(N)]
+MAP = [list(input().strip()) for _ in range(R)]
 
 direct = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
-root = set()
-result = 0
+lst = set()
+answer = 0
 
-def solution(x, y, cnt):
-    global result
-    if result < cnt:
-        result = cnt
-    if result == 26:
+def dfs(x, y, cnt):
+    global answer
+
+    if answer < cnt:
+        answer = cnt
+
+    if cnt == 26:
         print("26")
         exit(0)
 
-    for d in direct:
-        nx, ny = x + d[0], y + d[1]
-        if 0 <= nx < N and 0 <= ny < M and MAP[nx][ny] not in root:
-            root.add(MAP[nx][ny])
-            solution(nx, ny, cnt + 1)
-            root.remove(MAP[nx][ny])
+    for dx, dy in direct:
+        nx, ny = x + dx, y + dy
 
-root.add(MAP[0][0])
-solution(0, 0, 1)
-print(result)
+        if 0 <= nx < R and 0 <= ny < C and MAP[nx][ny] not in lst:
+            lst.add(MAP[nx][ny])
+            dfs(nx, ny, cnt + 1)
+            lst.remove(MAP[nx][ny])
+
+lst.add(MAP[0][0])
+dfs(0, 0, 1)
+print(answer)
