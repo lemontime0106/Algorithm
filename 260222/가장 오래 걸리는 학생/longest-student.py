f3c8a4fc76e5fe39b0b086e60960ffa1d47d4ad3
lemontime <139415938+lemontime0106@users.noreply.github.com>
2026-02-22 00:46:17 +0900

    Title: 가장 오래 걸리는 학생, Time: 555ms, Memory: 39MB, Status: Passed - Codetree

---
+++ 260222/가장 오래 걸리는 학생/longest-student.py
@@ -0,0 +1,36 @@
+import heapq
+
+N, M = map(int, input().split())
+
+MAP = [[] for _ in range(N+1)]
+
+for _ in range(M):
+    a, b, c = map(int, input().split())
+
+    MAP[a].append((b, c))
+    MAP[b].append((a, c))
+
+visited = [float("inf")] * (N+1)
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    visited[start] = 0
+
+    while q:
+        cost, now = heapq.heappop(q)
+
+        if cost > visited[now]:
+            continue
+
+        for nxt, weight in MAP[now]:
+            new_cost = weight + cost
+
+            if new_cost < visited[nxt]:
+                visited[nxt] = new_cost
+                heapq.heappush(q, (new_cost, nxt))
+    
+dijkstra(N)
+
+answer = max(visited[1:N])
+print(answer)
