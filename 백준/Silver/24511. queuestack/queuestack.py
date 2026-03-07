2a903ea157878d2a32808adef6146e12d4a59bdc
lemontime <139415938+lemontime0106@users.noreply.github.com>
2026-03-08 01:13:38 +0900

    [Silver III] Title: queuestack, Time: 188 ms, Memory: 137588 KB -BaekjoonHub

---
+++ 백준/Silver/24511. queuestack/queuestack.py
@@ -0,0 +1,21 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+dq = deque()
+
+for i in range(N):
+    if A[i] == 0:
+        dq.append(B[i])
+
+M = int(input())
+C = list(map(int, input().split()))
+
+for x in C:
+    dq.appendleft(x)
+    print(dq.pop(), end=" ")
