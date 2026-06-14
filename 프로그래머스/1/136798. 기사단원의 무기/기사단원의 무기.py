8473cc9a017a9ab0460e1659fba2bd7f47e501d6
lemontime <139415938+lemontime0106@users.noreply.github.com>
2026-06-15 00:01:42 +0900

    [level 1] Title: 기사단원의 무기, Time: 587.07 ms, Memory: 12.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -0,0 +1,22 @@
+def solution(number, limit, power):
+    answer = 0
+    lst = []
+    
+    for i in range(1, number + 1):
+        temp = 0
+        
+        for j in range(1, int(i ** 0.5) + 1):
+            if i % j == 0:
+                temp += 1
+                
+                if j != i // j:
+                    temp += 1
+        
+        if temp > limit:
+            lst.append(power)
+        else:
+            lst.append(temp)
+    
+    answer = sum(lst)
+    
+    return answer
