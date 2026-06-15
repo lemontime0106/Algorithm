bb8678eaa820eb7aea46986784860d8af14a0db8
lemontime <139415938+lemontime0106@users.noreply.github.com>
2026-06-15 22:27:13 +0900

    [level 1] Title: 카드 뭉치, Time: 0.03 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/README.md
+++ 프로그래머스/1/159994. 카드 뭉치/README.md
@@ -1,10 +1,10 @@
 # [level 1] 카드 뭉치 - 159994 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159994?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159994) 
 
 ### 성능 요약
 
-메모리: 9.32 MB, 시간: 0.01 ms
+메모리: 11.7 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 10일 23:53:32
+2026년 06월 15일 22:27:08
 
 ### 문제 설명
 
--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -1,12 +1,18 @@
-def check(lst1, lst2, word):
-    for w in word:
-        if lst1 and lst1[0] == w:
-            lst1.pop(0)
-        elif lst2 and lst2[0] == w:
-            lst2.pop(0)
-        else:
-            return "No"
-    return "Yes"
-
 def solution(cards1, cards2, goal):
-    return check(cards1, cards2, goal)
+    answer = 'Yes'
+    
+    flag = True
+    
+    for i in goal:
+        if cards1 and cards1[0] == i:
+            cards1.pop(0)
+        elif cards2 and cards2[0] == i:
+            cards2.pop(0)
+        else:
+            flag = False
+        
+        if not flag:
+            answer = "No"
+            break
+    
+    return answer
