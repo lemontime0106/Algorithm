f2bf63b299ffc37d646aade3e070a7932788992b
lemontime <139415938+lemontime0106@users.noreply.github.com>
2026-06-27 00:21:49 +0900

    [level 1] Title: 햄버거 만들기, Time: 88.93 ms, Memory: 28 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -0,0 +1,13 @@
+def solution(ingredient):
+    answer = 0
+    stack = []
+
+    for x in ingredient:
+        stack.append(x)
+
+        if stack[-4:] == [1, 2, 3, 1]:
+            answer += 1
+            for _ in range(4):
+                stack.pop()
+
+    return answer
