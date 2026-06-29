763902275badd42d34967d90df6bf57e9500b99e
lemontime <139415938+lemontime0106@users.noreply.github.com>
2026-06-29 22:02:33 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,10 +1,9 @@
 def solution(brown, yellow):
     total = brown + yellow
     
-    # total의 약수를 구해서 가능한 가로, 세로를 확인
-    for height in range(3, int(total**0.5) + 1):
-        if total % height == 0:
-            width = total // height
-            # 갈색 타일과 노란색 타일의 조건을 만족하는지 확인
-            if (width - 2) * (height - 2) == yellow:
-                return [width, height]
+    for h in range(3, int(total ** 0.5) + 1):
+        if total % h == 0:
+            w = total // h
+            
+            if (w-2) * (h-2) == yellow:
+                return [w, h]
