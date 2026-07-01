1d1ebe3f354bac42f7cb44ae733247b99192ad9d
lemontime <139415938+lemontime0106@users.noreply.github.com>
2026-07-01 22:56:35 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/README.md
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/README.md
@@ -4,7 +4,7 @@
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 0.03 ms
+메모리: 11.4 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,11 +16,11 @@
 
 ### 제출 일자
 
-2024년 11월 16일 00:46:51
+2026년 07월 01일 22:56:27
 
 ### 문제 설명
 
-<p><img src="https://grepp-programmers.s3.ap-northeast-2.amazonaws.com/files/production/d31cb063-4025-4412-8cbc-6ac6909cf93e/img1.png" title="" alt="img1.png"></p>
+<p><img src="https://asset.programmers.co.kr/files/production/d31cb063-4025-4412-8cbc-6ac6909cf93e/img1.png" title="" alt="img1.png"></p>
 
 <p>네오와 프로도가 숫자놀이를 하고 있습니다. 네오가 프로도에게 숫자를 건넬 때 일부 자릿수를 영단어로 바꾼 카드를 건네주면 프로도는 원래 숫자를 찾는 게임입니다.<br><br>
 다음은 숫자의 일부 자릿수를 영단어로 바꾸는 예시입니다.</p>
--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,17 +1,27 @@
 def solution(s):
-    num_word = {"zero": "0", "one": "1", "two": "2", "three": "3", "four": "4", 
-                "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
-    
-    temp = ""
     answer = ""
     
-    for char in s:
-        if char.isdigit():
-            answer += char
-        else:
-            temp += char
-            if temp in num_word:
-                answer += num_word[temp]
-                temp = ""
+    s = list(s)
     
-    return int(answer)
+    number = {
+        "one" : 1, "two" : 2, "three" : 3,
+        "four" : 4, "five" : 5, "six" : 6,
+        "seven" : 7, "eight" : 8, "nine" : 9, "zero" : 0
+    }
+    
+    temp = ""
+    
+    while s:
+        a = s.pop(0)
+        
+        if a.isdigit():
+            answer += a
+        else:
+            temp += a
+            if temp in number:
+                answer += str(number[temp])
+                temp = ""
+
+    answer = int(answer)
+    
+    return answer
