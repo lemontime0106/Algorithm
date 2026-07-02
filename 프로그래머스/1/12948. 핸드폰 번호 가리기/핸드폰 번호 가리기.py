c2a3f20fa8fb2d7495e2354b15fd489123631201
lemontime <139415938+lemontime0106@users.noreply.github.com>
2026-07-03 00:05:51 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
@@ -0,0 +1,12 @@
+def solution(phone_number):
+    answer = ''
+    
+    len_number = len(phone_number)
+    
+    for i in range(len_number):
+        if i < len_number - 4:
+            answer += "*"
+        else:
+            answer += str(phone_number[i])
+    
+    return answer
