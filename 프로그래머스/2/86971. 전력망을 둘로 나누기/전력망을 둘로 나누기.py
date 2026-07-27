ab530e68c4dc0ac7e263b57f08bd210f4de56123
lemontime <139415938+lemontime0106@users.noreply.github.com>
2026-07-28 00:08:20 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.02 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,34 +1,42 @@
+from collections import deque
+
 def solution(n, wires):
     answer = float("inf")
-    
-    def bfs(start):
-        q = []
-        q.append(start)
-        visited = [False] * (n + 1)
-        visited[start] = True
-        cnt = 1
-        while q:
-            v = q.pop(0)
-            for k in graph[v]:
-                if not visited[k]:
-                    visited[k] = True
-                    q.append(k)
-                    cnt += 1
-        return cnt
-    
+
     for i in range(len(wires)):
-        graph = [[] for _ in range(n + 1)]
+        MAP = [[] for _ in range(n + 1)]
+
         for j in range(len(wires)):
             if i == j:
                 continue
-            
-            a, b = wires[j][0], wires[j][1]
-            graph[a].append(b)
-            graph[b].append(a)
-        
+
+            a, b = wires[j]
+            MAP[a].append(b)
+            MAP[b].append(a)
+
+        def bfs(start):
+            q = deque([start])
+
+            visited = [False] * (n + 1)
+            visited[start] = True
+
+            cnt = 1
+
+            while q:
+                v = q.popleft()
+
+                for next_node in MAP[v]:
+                    if not visited[next_node]:
+                        visited[next_node] = True
+                        q.append(next_node)
+                        cnt += 1
+
+            return cnt
+
         group1 = bfs(1)
+
         group2 = n - group1
-        answer = min(answer, abs(n - (group1 * 2)))
-        
-    
+
+        answer = min(answer, abs(group1 - group2))
+
     return answer
