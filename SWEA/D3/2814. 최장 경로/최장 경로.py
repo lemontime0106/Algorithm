e010a215e127158cfa1b80cee8f72342af865dac
lemontime <139415938+lemontime0106@users.noreply.github.com>
2026-08-05 19:50:26 +0900

    [D3] Title: 최장 경로, Time: 81 ms, Memory: 60,800 KB -BaekjoonHub

---
+++ SWEA/D3/2814. 최장 경로/최장 경로.py
@@ -0,0 +1,32 @@
+def dfs(node, cnt):
+    global answer
+    
+    answer = max(answer, cnt)
+    
+    for next_node in graph[node]:
+        if not visited[next_node]:
+            visited[next_node] = True
+            dfs(next_node, cnt+1)
+            visited[next_node] = False
+            
+
+for t in range(1, int(input())+1):
+    N, M = map(int, input().split())
+
+    graph = [[] for _ in range(N+1)]
+
+    for _ in range(M):
+        x, y = map(int, input().split())
+
+        graph[x].append(y)
+        graph[y].append(x)
+        
+    answer = 1
+    
+    for start in range(1, N+1):
+        visited = [False] * (N+1)
+        visited[start] = True
+        
+        dfs(start, 1)
+    
+    print(f"#{t} {answer}")
