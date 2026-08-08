66043fd3e241a87c6a0e8cea63e801517fcb8d37
lemontime <139415938+lemontime0106@users.noreply.github.com>
2026-08-09 00:24:18 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 117 ms, Memory: 62,592 KB -BaekjoonHub

---
+++ SWEA/D4/7465. 창용 마을 무리의 개수/README.md
@@ -0,0 +1,15 @@
+# [D4] 창용 마을 무리의 개수 - 7465 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWngfZVa9XwDFAQU) 
+
+### 성능 요약
+
+메모리: 62,592 KB, 시간: 117 ms, 코드길이: 748 Bytes
+
+### 제출 일자
+
+2026-08-09 00:24
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.py
@@ -0,0 +1,37 @@
+# 7465. 창용 마을 무리의 개수
+
+from collections import deque
+
+for t in range(1, int(input())+1):
+    N, M = map(int, input().split())
+
+    graph = [[] for _ in range(N+1)]
+
+    for _ in range(M):
+        a, b = map(int, input().split())
+
+        graph[a].append(b)
+        graph[b].append(a)
+
+    visited = [False] * (N+1)
+    answer = 0
+
+    for i in range(1, N+1):
+        if visited[i]:
+            continue
+
+        answer += 1
+
+        q = deque()
+        q.append(i)
+        visited[i] = True
+
+        while q:
+            node = q.popleft()
+
+            for next_node in graph[node]:
+                if not visited[next_node]:
+                    visited[next_node] = True
+                    q.append(next_node)
+
+    print(f"#{t} {answer}")
