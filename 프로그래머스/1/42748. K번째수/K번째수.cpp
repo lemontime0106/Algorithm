a16e921f4f4fb109ccad195c8af71be90e888df4
lemontime <139415938+lemontime0106@users.noreply.github.com>
2026-09-08 00:06:31 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 4.95 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+    
+    for (vector<int> command : commands) {
+        int i = command[0];
+        int j = command[1];
+        int k = command[2];
+        
+        vector<int> temp;
+        
+        
+        for (int idx=i-1; idx < j; idx++) {
+            temp.push_back(array[idx]);
+        }
+        
+        sort(temp.begin(), temp.end());
+        
+        answer.push_back(temp[k-1]);
+    }
+        
+    
+    return answer;
+}
--- 프로그래머스/1/42748. K번째수/README.md
+++ 프로그래머스/1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=cpp) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.10 ms
+메모리: 4.95 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 12월 22일 22:27:50
+2026년 09월 08일 00:06:10
 
 ### 문제 설명
 
