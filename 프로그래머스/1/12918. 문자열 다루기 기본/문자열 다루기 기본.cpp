e7b597e9fcd866dfb5d5ab043441b04b79562111
lemontime <139415938+lemontime0106@users.noreply.github.com>
2026-09-09 00:06:31 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 5.07 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(string s) {
+    if (s.size() != 4 && s.size() != 6) {
+        return false;
+    }
+    
+    for (char c : s) {
+        if (c < '0' || c > '9') {
+            return false;
+        }
+    }
+    
+    return true;
+}
