7c19d3ad5a22083e58cd18cf7893378d0e9bc276
lemontime <139415938+lemontime0106@users.noreply.github.com>
2026-09-10 00:01:02 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 4.64 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862?language=cpp) 
 
 ### 성능 요약
 
-메모리: 11.4 MB, 시간: 0.01 ms
+메모리: 4.64 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 06월 21일 14:12:25
+2026년 09월 10일 00:00:37
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/42862. 체육복/체육복.cpp
@@ -0,0 +1,40 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, vector<int> lost, vector<int> reserve) {
+    vector<int> clothes(n+1, 1);
+    
+    for (int student : lost) {
+        clothes[student]--;
+    } 
+    
+    for (int student : reserve) {
+        clothes[student]++;
+    }
+    
+    for (int i = 1; i <= n; i++) {
+        if (clothes[i] == 0) {
+            if (i > 1 && clothes[i-1] == 2) {
+                clothes[i-1]--;
+                clothes[i]++;
+            }
+            
+            else if (i < n && clothes[i+1] == 2) {
+                clothes[i+1]--;
+                clothes[i]++;
+            }
+        }
+    }
+    
+    int answer = 0;
+    
+    for (int i = 1; i <= n; i++) {
+        if (clothes[i] >= 1) {
+            answer++;
+        }
+    }
+    
+    return answer;
+}
